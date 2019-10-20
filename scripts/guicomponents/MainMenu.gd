4fc02340a2299b9a7a22ce9e0ef42b690751d144
James <jamoxploder@gmail.com>
2019-10-20 10:37:03 +0200

    HostTab is is now functional with basic validation

--- scripts/guicomponents/MainMenu.gd
+++ scripts/guicomponents/MainMenu.gd
@@ -1,13 +1,16 @@
 extends MarginContainer
 
 func _on_HostGame_pressed():
-	print("Host Game pressed")
+	$PanelContainer/RightPanelContainer/TabContainer.set_current_tab(0)
 
 func _on_JoinGame_pressed():
-	print("Join Game pressed")
+	$PanelContainer/RightPanelContainer/TabContainer.set_current_tab(1)
 
 func _on_Settings_pressed():
-	print("Settings pressed")
+	$PanelContainer/RightPanelContainer/TabContainer.set_current_tab(2)
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+func _on__Host_Game__hostgame(port, password):
+	print("hosting game at port: " + str(port) + " with password: " + password)
