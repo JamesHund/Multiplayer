709a13d3fc904605107fa09904a31055997feb20
James <jamoxploder@gmail.com>
2019-10-25 19:42:55 +0200

    Peers can connect and basic lobby gui

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const MAX_CLIENTS = 5
 var server_password
+var local_ip
 
 func _ready():
 	$GUIManager.show_MainMenu()
@@ -13,10 +14,16 @@ func init_server(port,password):
 	get_tree().set_network_peer(peer)
 	server_password = password
 	$LevelManager.load_world("DefaultWorld")
+	get_tree().connect("network_peer_connected", self, "player_connected")
+	print("initialized as server with public ip: " + str(IP.get_local_addresses()[2]))
+	print("local ip: " + str(IP.get_local_addresses()[1]))
 	
 func init_client(ip, port, password):
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(ip, port)
 	get_tree().set_network_peer(peer)
+	
+func player_connected(id):
+	print("player: " + str(id) + " connected") 
 	
 
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,13 +11,15 @@ func _physics_process(delta):
 
 func get_input():
 	velocity = Vector2(0,0)
-	if Input.is_action_pressed("ui_down"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	velocity = velocity.normalized() * speed
+	
+	if is_network_master():
+		if Input.is_action_pressed("ui_down"):
+			velocity.y -= 1
+		if Input.is_action_pressed("ui_up"):
+			velocity.y += 1
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		velocity = velocity.normalized() * speed
 	
