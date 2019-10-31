74dd2e0fff1bd86f74b25036d121ef0417c6dbe7
James <jamoxploder@gmail.com>
2019-10-31 20:37:30 +0200

    stuff

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,6 +3,8 @@ extends Node2D
 const MAX_CLIENTS = 5
 var server_password
 var local_ip
+var my_info
+var players
 
 func _ready():
 	$GUIManager.show_MainMenu()
@@ -22,8 +24,12 @@ func init_client(ip, port, password):
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(ip, port)
 	get_tree().set_network_peer(peer)
+	my_info = SettingsData.player_name
 	
 func player_connected(id):
-	print("player: " + str(id) + " connected") 
+	print("player: " + str(id) + " connected")
+	rpc_id(id,"register_player", my_info)
 	
-
+remote func register_player(info):
+	var id = get_tree().get_rpc_sender_id()
+	players[id] = info
